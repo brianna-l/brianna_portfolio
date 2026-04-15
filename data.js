324b41976d7e39d6dc63b8440c47c1db1bbbe018
brianna-l <loub697@newschool.edu>
2026-04-14 20:01:47 -0400

    pp

--- data.js
+++ data.js
@@ -1,26 +1,58 @@
 const projects = [
   {
     title: "With Habit",
-    time: "2025",
+    time: "2026",
     categories: ["Product", "Print"],
-    image: "media/withhabit-1.jpg",
-    link: "project2.html",
+    image: "/brianna_portfolio/media/withhabit-1.jpg",
+    link: "habit.html",
     alt: "With Habit cover image"
+  },
+  {
+    title: "Moma PSX",
+    time: "2025",
+    categories: ["Branding", "Digital"],
+    image: "/brianna_portfolio/media/psx-1.jpg",
+    link: "project2.html",
+    alt: "Moma PSX cover image"
+  },
+  {
+    title: "Echo",
+    time: "2025",
+    categories: ["Connection", "Digital", "App"],
+    image: "/brianna_portfolio/media/echo-1.jpg",
+    link: "echo.html",
+    alt: "Echo cover image"
+  },
+  {
+    title: "Upcyclo",
+    time: "2025",
+    categories: ["Connection", "Digital", "Print"],
+    image: "/brianna_portfolio/media/upcyclo-1.jpg",
+    link: "upcyclo.html",
+    alt: "Upcyclo cover image"
+  },
+  {
+    title: "Music+",
+    time: "2024",
+    categories: ["Media", "Digital", "App"],
+    image: "/brianna_portfolio/media/music1.jpg",
+    link: "music.html",
+    alt: "Music+ cover image"
   },
   {
     title: "Inside Out",
     time: "2023",
     categories: ["Nature", "Digital"],
-    image: "media/one.png",
-    link: "project1.html",
+    image: "/brianna_portfolio/media/one.png",
+    link: "inout.html",
     alt: "Inside Out cover image"
   },
   {
-    title: "Shadow",
+    title: "Mirrored Universe",
     time: "2023",
-    categories: ["Film", "Digital"],
-    image: "media/two.png",
-    link: "project2.html",
-    alt: "Shadow cover image"
+    categories: ["Film", "Digital", "Print"],
+    image: "/brianna_portfolio/media/universe.jpg",
+    link: "universe.html",
+    alt: "Mirrored Universe cover image"
   }
 ];
